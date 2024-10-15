168cc5d2137758cefc98619caf840159b39534e4
MutiaMarjuliPutri <162146690+MutiaMarjuliPutri@users.noreply.github.com>
2024-10-15 21:03:42 +0700

    Initial commit

---
+++ app/src/main/java/com/mutia/klinikapp/adapter/AdapterMenuIcon.kt
@@ -0,0 +1,37 @@
+package com.mutia.klinikapp.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.mutia.klinikapp.R
+import com.mutia.klinikapp.model.ModelIcon
+
+
+class AdapterMenuIcon (
+    val itemMenuList: List<ModelIcon>
+): RecyclerView.Adapter<AdapterMenuIcon.MyviewHolder>(){
+    class MyviewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        var imgMenuIcon : ImageView = itemView.findViewById(R.id.imgMenuIcon)
+        var txtNamaMenu : TextView = itemView.findViewById(R.id.txtNamaMenu)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyviewHolder {
+        val nView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_menu,parent,false)
+        return MyviewHolder(nView)
+    }
+
+    override fun getItemCount(): Int {
+        return itemMenuList.size
+    }
+
+    override fun onBindViewHolder(holder: MyviewHolder, position: Int) {
+        val currentItem = itemMenuList[position]
+        holder.imgMenuIcon.setImageResource(currentItem.icon)
+        holder.txtNamaMenu.setText(currentItem.iconName)
+    }
+
+}
---
+++ app/src/main/java/com/mutia/klinikapp/model/ModelIcon.kt
@@ -0,0 +1,7 @@
+package com.mutia.klinikapp.model
+
+data class ModelIcon(
+var icon : Int,
+var iconName : String
+)
+
